187e99987e6a50d6336a333327224b88639f14c7
David Misshula <david.misshula@gmail.com>
2013-02-11 17:18:22 -0500

    fixing the transitions

--- public/app/views/index.js
+++ public/app/views/index.js
@@ -9,17 +9,24 @@ function(app){
 			'click #nav_about' : 'about',
 			'click #nav_contact' : 'contact',
 			'click #nav_schedule' : 'schedule',
-			'click #nav_heart' : 'heart'
+			'click #nav_heart' : 'heart',
+			'click #nav_home' : 'home'
 		},
 		el : 'body',
 
+		home : function(){
+			$('.module').fadeOut(1500);
+			$('#home').fadeIn(1500);
+			$('#homemodal').fadeIn(1500);
+		},
+
 		about : function(){
-			$('.module').hide();
+			$('.module').fadeOut(1500);
 			$('#about').fadeIn(1500);
 		},
 
 		contact : function(){
-			$('.module').hide();
+			$('.module').fadeOut(1500);
 			$('#contact').fadeIn(1500);
 			this.model.save({
 				success : function(){
@@ -29,18 +36,19 @@ function(app){
 		},
 
 		schedule : function(){
-			$('.module').hide();
+			$('.module').fadeOut(1500);
 			$('#schedule').fadeIn(1500);
 		},
 
 		heart : function(){
-			$('.module').hide();
+			$('.module').fadeOut(1500);
 			$('#heart').fadeIn(1500);
 		},
 
 		render : function(){
 
 			$('#home').show()
+			$('#homemodal').fadeIn(1500)
 			
 			$('#home').backstretch('/images/IMG_4222.jpg');
 			$('#about').backstretch('/images/IMG_4383.jpg');
